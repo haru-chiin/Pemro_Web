c3ef2cfe18351ccd93127606a7e2c7b9c71d3685
Haruuu <107801206+haru-chiin@users.noreply.github.com>
2023-10-24 19:26:18 +0700

    Add files via upload

---
+++ src/pages/aboutus.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Us Page</h2>
+      <p>Halo ini about us ges ya</p>
+    </div>
+  );
+};
+
+export default AboutUs;
